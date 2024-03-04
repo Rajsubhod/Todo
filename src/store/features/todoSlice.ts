86d4d490fa5817bc2370696758a74cd1ea73c98f
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-03-04 17:46:59 +0530

    Refactor todoSlice and added hooks

---
+++ src/store/features/todoSlice.ts
@@ -0,0 +1,62 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+interface todo {
+	id: string;
+	body: string;
+	done: boolean;
+}
+
+interface todoState {
+	todoList: todo[];
+}
+
+// interface todoState {
+// 	todoList: {
+// 		id: 0;
+// 		body: string;
+// 		done: boolean;
+// 	}[];
+// }
+
+const initialState: todoState = {
+	todoList: [],
+};
+
+const todoSlice = createSlice({
+	name: 'todo',
+	initialState,
+	reducers: {
+		addTodo: (state, action: PayloadAction<{ id: string; body: string }>) => {
+			state.todoList.push({
+				id: action.payload.id,
+				body: action.payload.body,
+				done: false,
+			});
+		},
+		updateTodo: (
+			state,
+			action: PayloadAction<{ id: string; body: string }>
+		) => {
+			state.todoList = state.todoList.map((todo) =>
+				todo.id === action.payload.id
+					? { ...todo, body: action.payload.body }
+					: todo
+			);
+		},
+		deleteTodo: (state, action: PayloadAction<{ id: string }>) => {
+			state.todoList = state.todoList.filter(
+				(todo) => todo.id !== action.payload.id
+			);
+		},
+		toggleDone: (state, action: PayloadAction<{ id: string }>) => {
+			state.todoList = state.todoList.map((todo) =>
+				todo.id === action.payload.id ? { ...todo, done: !todo.done } : todo
+			);
+		},
+	},
+});
+
+export const { addTodo, deleteTodo, updateTodo, toggleDone } =
+	todoSlice.actions;
+
+export default todoSlice.reducer;
